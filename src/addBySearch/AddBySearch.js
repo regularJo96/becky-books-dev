21c16c47b5e09073e6b4867d3626d84931cddff9
Josiah Anderson <riddlerforever@live.com>
2024-11-16 19:08:17 -0600

    trying new error mess

--- src/addBySearch/AddBySearch.js
+++ src/addBySearch/AddBySearch.js
@@ -32,8 +32,8 @@ function AddBySearch(props){
     })
       .then(response => {
         if (!response.ok) {
-          throw new Error('Network response was not ok ' + response.statusText);
           setSearchStr(response);
+          throw new Error('Network response was not ok ' + response.statusText);
         }
         
         return response.json();
