86423cad18c569e06f7e86920ee096773c18d182
Josiah Anderson <riddlerforever@live.com>
2024-12-02 11:32:25 -0600

    updated style of book manual add form, changed labels to floating labels

--- src/addBookManually/AddBookManually.js
+++ src/addBookManually/AddBookManually.js
@@ -41,20 +41,31 @@ const addBook = (event) => {
 
     return(
         <>
-            <div className="container">
-                <div className="form-title">Add Book Manually</div>
+            <div className="container bg-blue">
+                
                 <form className="add-book-custom-form" onSubmit={addBook}>
-                    <input style={formStyle} type="text" placeholder='Add Title' value={bookTitle} onChange={e => setBookTitle(e.target.value)}/>
-                    <input style={formStyle} type="text" placeholder='Add Author' value={bookAuthor} onChange={e => setBookAuthor(e.target.value)}/>
-                    <input style={formStyle} type="text" placeholder='Add Description' value={bookDescription} onChange={e => setBookDescription(e.target.value)}/>
-                    <label>
-                        <select classname="row justify-content-center" id="shelfSelect" value={bookShelf} onChange={e => setBookShelf(e.target.value)}>
-                            <option value="">Select Shelf</option>
-                            <option value={"favorite"}>To Read</option>
-                            <option value={"menu_book"}>Am Reading</option>
-                            <option value={"check_circle"}>Have Read</option>
-                        </select>
-                    </label>
+                    <div className="form-title text-white bg-wine">Add Book Manually</div>
+                    <div className="form-floating w-100">
+                        <input id="book-title" className="form-control" style={formStyle} type="text" placeholder='Add Title' value={bookTitle} onChange={e => setBookTitle(e.target.value)}/>
+                        <label for="book-title">Title</label>
+                    </div>
+
+                    <div className="form-floating w-100">
+                        <input id="book-author" className="form-control" style={formStyle} type="text" placeholder='Add Author' value={bookAuthor} onChange={e => setBookAuthor(e.target.value)}/>
+                        <label for="book-author">Author</label>
+                    </div>
+                    <div className="form-floating w-100">
+                        <input id="book-description" className="form-control" style={formStyle} type="text" placeholder='Add Description' value={bookDescription} onChange={e => setBookDescription(e.target.value)}/>
+                        <label for="book-description">Description</label>
+                    </div>
+                    
+                    <select classname="row justify-content-center" id="shelfSelect" value={bookShelf} onChange={e => setBookShelf(e.target.value)}>
+                        <option value="">Select Shelf</option>
+                        <option value={"favorite"}>To Read</option>
+                        <option value={"menu_book"}>Am Reading</option>
+                        <option value={"check_circle"}>Have Read</option>
+                    </select>
+
                     <button type="submit" className={`button border-latte text-white text-center pointer ${buttonHighlight}`} onMouseEnter={e => {handleHighlight(e.target, true)}} onMouseLeave={e => {handleHighlight(e.target, false)}} onTouchEnd={e => {handleHighlight(e.target, false)}}>Add Book</button>
                 </form>
             </div>
