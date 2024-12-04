706b1ff022f26de79062f2471cce55203655bbcf
Josiah Anderson <riddlerforever@live.com>
2024-12-03 19:36:53 -0600

    BUGFIX: fixed error where shelf with 'no books' is still visible on form open

--- src/shelf/Shelf.js
+++ src/shelf/Shelf.js
@@ -30,7 +30,7 @@ function Shelf(props){
   if(props.books.length == 0){
     return(
       <>
-        <div id="shelf" className="text-center no-books bg-latte text-xlarge text-white">
+        <div id="shelf" className={`text-center no-books bg-latte text-xlarge text-white  ${shelfHide}`}>
             no books here
         </div>
       </>
